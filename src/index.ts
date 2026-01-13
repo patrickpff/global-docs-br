27f241ec80df6867e580c72d4724a05411cb2045
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2026-01-13 17:48:45 -0300

    patching: exporting documents

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,13 @@ export * from "global-docs";
 export * from "./documents/cau/cau.js";
 export * from "./documents/cnh/cnh.js";
 export * from "./documents/cnpj/cnpj.js";
+export * from "./documents/cpf/cpf.js";
 export * from "./documents/crc/crc.js";
 export * from "./documents/crea/crea.js";
 export * from "./documents/crm/crm.js";
 export * from "./documents/cro/cro.js";
 export * from "./documents/oab/oab.js";
 export * from "./documents/passport/passport.js";
+export * from "./documents/process-cnj/process-cnj.js"
 export * from "./documents/rg/rg.js";
 export * from "./documents/voter-registration/voter-registration.js";
